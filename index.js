e9f28a181af71dc48741d27cfc4004277420a095
FilipeScotti <gringo.vm91@gmail.com>
2023-04-27 08:59:29 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,76 @@
+// const got = "game of thrones"
+// console.log (typeof got)
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+
+
+
+
+
+// const idade = 20
+
+// console.log('Idade é',idade);
+
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+//------------------------------------------------------------------------------
+
+// let nome
+// let idade
+
+
+
+// idade = Number(prompt ("qual a sua idade"))
+// nome = String(prompt ("qual é o seu nome"))
+
+// console.log (typeof idade)
+// console.log(typeof nome)
+// console.log("olá,",nome,"você tem",idade, "anos")
+
+
+// let trabalho = prompt("você trabalha, sim ou nao")
+// let estuda = prompt ("você estuda, sim ou não")
+// let transporte = prompt("você usa transporte, sim ou não")
+
+// console.log (trabalho,estuda,transporte)
+
+
+//const pergunta = prompt("voce comeu hoje?(sim/nao)")
+
+//console.log("primeira pergunta",pergunta);
+
+
+
+
+let a = Number(prompt("gigite um numero"))
+let b = Number(prompt ("digite outro numero"))
+
+let c = b
+b = a 
+a = c
+
+console.log("valor do primeiro numero",a);
+console.log("valor do primeiro numero",b);
+
+
+
+
+
+
+
+
+
+
